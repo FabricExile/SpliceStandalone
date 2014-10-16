b8a84a94506ecb8796115561c2c913c035fd9c25
Helge Mathee <helge.mathee@fabricengine.com>
2014-10-16 15:03:52 +0200

    #3245

--- src/SpliceStandalone.cpp
+++ src/SpliceStandalone.cpp
@@ -244,26 +244,29 @@ void SpliceStandalone::needRedraw()
 void SpliceStandalone::setupFusionLook()
 {
   qApp->setStyle(QStyleFactory::create("Fusion"));
+
+  // hmathee: This was causing the app to show up badly on some
+  // operating systems. going for the Qt standard style now.
+
+  // QPalette darkPalette;
+  // darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+  // darkPalette.setColor(QPalette::WindowText, Qt::white);
+  // darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+  // darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+  // darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+  // darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+  // //darkPalette.setColor(QPalette::Text, Qt::white);
+  // darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+  // darkPalette.setColor(QPalette::ButtonText, Qt::white);
+  // darkPalette.setColor(QPalette::BrightText, Qt::red);
+  // darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
    
-  QPalette darkPalette;
-  darkPalette.setColor(QPalette::Window, QColor(53,53,53));
-  darkPalette.setColor(QPalette::WindowText, Qt::white);
-  darkPalette.setColor(QPalette::Base, QColor(25,25,25));
-  darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
-  darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
-  darkPalette.setColor(QPalette::ToolTipText, Qt::white);
-  darkPalette.setColor(QPalette::Text, Qt::white);
-  darkPalette.setColor(QPalette::Button, QColor(53,53,53));
-  darkPalette.setColor(QPalette::ButtonText, Qt::white);
-  darkPalette.setColor(QPalette::BrightText, Qt::red);
-  darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
-   
-  darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-  darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+  // darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+  // darkPalette.setColor(QPalette::HighlightedText, Qt::black);
        
-  qApp->setPalette(darkPalette);
+  // qApp->setPalette(darkPalette);
    
-  qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+  // qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 
 }
 
